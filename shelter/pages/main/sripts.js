efd2d3260379acd3ae19bcbb0f8ecf8da62ad266
Artsiom Kasmatskou <artemkosmatkov1994@gmail.com>
2023-04-08 15:16:46 +0300

    feat: implement burger menu with animation in main

--- shelter/pages/main/sripts.js
+++
@@ -1,8 +0,0 @@
-// burger menu
-const burgerButton = document.querySelector('.burger');
-const navHeader = document.querySelector('.menu')
-
-burgerButton.addEventListener('click', (event) => {
-  burgerButton.classList.toggle('burger_active')
-  navHeader.classList.toggle('menu_active')
-})
