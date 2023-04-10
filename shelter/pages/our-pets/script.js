a8295b375d3a3149309e841cf8c7e9417cad96f5
Artsiom Kasmatskou <artemkosmatkov1994@gmail.com>
2023-04-10 16:24:20 +0300

    fix: add separate mobile menu in 'our pets'

--- shelter/pages/our-pets/script.js
+++ shelter/pages/our-pets/script.js
@@ -1,23 +1,35 @@
+import PETS from '../../assets/info_pets.json' assert { type:'json' }
+const initialPets = PETS
+console.log(initialPets)
+
+
 // burger menu
 const burgerButton = document.querySelector('.burger')
-const navHeader = document.querySelector('.menu')
+const navHeader = document.querySelector('.mobile-menu')
 const notScroll = document.getElementById('html')
 const overLay = document.querySelector('.overlay')
-const menuLinkItem = document.querySelectorAll('.menu__item')
+const menuLinkItem = document.querySelectorAll('.mobile-menu__item')
 
-function closeOpenBurgerHandler() {
+function menuBurgerActivityHandler() {
   burgerButton.classList.toggle('burger_active')
-  navHeader.classList.toggle('menu_active')
+  navHeader.classList.toggle('mobile-menu_active')
   notScroll.classList.toggle('lock-scroll')
   overLay.classList.toggle('overlay_active')
 }
 
+// open burger menu when click on icon burger
 burgerButton.addEventListener('click', (event) => {
-  closeOpenBurgerHandler()
+  menuBurgerActivityHandler()
 })
 
+//close burger menu when click on link
 menuLinkItem.forEach((link) => {
   link.addEventListener('click', (event) => {
-    closeOpenBurgerHandler()
+    menuBurgerActivityHandler()
   })
+})
+
+//close burger menu when click outside
+overLay.addEventListener('click', (event) => {
+  menuBurgerActivityHandler()
 })
